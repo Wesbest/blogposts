7ad1dc40525a0136217e47985bf15d8e4042b790
Wesley Rouw <wesleyrouw@MacBook-Pro-van-Wesley.local>
2022-11-11 15:55:16 +0100

    Initial commit

--- blogposts_test.go
+++ blogposts_test.go
@@ -1,34 +1,19 @@
 package blogposts_test
 
 import (
-	"errors"
-	"io/fs"
 	"testing"
 	"testing/fstest"
-
-	"github.com/Wesbest/blogposts"
 )
 
 func TestNewBlogPosts(t *testing.T) {
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
+		"hello world.md" {Data: []byte("hi")},
+		"hello-world2.md" {Data: []byte("hola")},
 	}
 
-	posts, err := blogposts.NewPostsFromFS(fs)
-
-	if err != nil {
-		t.Fatal(err)
-	}
+	posts := blogposts.NewPostsFromFs(fs)
 
 	if len(posts) != len(fs) {
 		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
 	}
-}
-
-type StubFailingFS struct {
-}
-
-func (s StubFailingFS) Open(name string) (fs.File, error) {
-	return nil, errors.New("oh no, I always fail")
 }
